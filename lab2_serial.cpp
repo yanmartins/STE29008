790880cfa4f45b66805af333619f069c1bc40656
Yan Lucas Martins <30724634+yanmartins@users.noreply.github.com>
2019-03-31 14:42:05 -0300

    Update lab2_serial.cpp

--- lab2_serial.cpp
+++ lab2_serial.cpp
@@ -2,11 +2,11 @@
 #include <util/delay.h>
 
 #define FOSC 16000000			// Define velocidade do clock
-#define BAUD 9600				// Define baudrate
+#define BAUD 9600			// Define baudrate
 #define MYUBRR (FOSC/16/BAUD-1) // Define a frequência de oscilação
 
 void USART_Init( unsigned int ubrr){
-	UBRR0 = ubrr;						// Define a frequência de oscilação
+	UBRR0 = ubrr;				// Define a frequência de oscilação
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0); 	// Habilita RX e TX
 	UCSR0C = (1<<USBS0)|(3<<UCSZ00); 	// Define formato do frame: 8 data, 2 stop bit
 }

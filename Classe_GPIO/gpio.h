fd26ad0a96627c4e03685309e8432263df214e9d
Yan Lucas Martins <30724634+yanmartins@users.noreply.github.com>
2019-04-03 15:13:26 -0300

    Create gpio.h

---
+++ Classe_GPIO/gpio.h
@@ -0,0 +1,32 @@
+/*
+ * gpio.h
+ *
+ *  Created on: 03/04/2019
+ *      Author: aluno
+ */
+
+#include <avr/io.h>
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+class GPIO {
+public:
+	enum PortDirection_t{
+		INPUT = 0,
+		OUTPUT = 1
+	};
+
+	GPIO (uint8_t id, PortDirection_t dir);
+	~GPIO();
+
+	bool read();
+	void write(bool val);
+	void toggle();
+
+private:
+	uint8_t _bit; // valor no datasheet
+	uint8_t _id; // valor no arduino
+};
+
+#endif /* GPIO_H_ */

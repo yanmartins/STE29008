ba2fa57fcd0c19211997684b32273c4ef6ae2a12
Yan Lucas Martins <30724634+yanmartins@users.noreply.github.com>
2019-04-24 15:15:31 -0300

    Implementando ISR e correção no construtor

--- Classe_UART/versao2/uart.cpp
+++ Classe_UART/versao2/uart.cpp
@@ -9,12 +9,14 @@
 #include <avr/interrupt.h>
 #include "uart.h"
 
+bool UART::_has_data = false;
+uint8_t UART::_rx_buffer = 0;
+uint8_t UART::_tx_buffer = 0;
 
 UART::UART(uint32_t baud, DATABITS_t db, PARITY_t parity, STOPBITS_t sb) {
 	_has_data = false;
 	UBRR0 = (16000000/16/baud-1);
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-	UCSR0B = (1<<RXCIE0);
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
 	UCSR0C = db|parity|sb;
 }
 
@@ -35,14 +37,21 @@ bool UART::has_data(){
 	return _has_data;
 }
 
-void UART::rx_isr_handler(USART0_RX_vect){
+
+void UART::rx_isr_handler(){
 	_rx_buffer = UDR0;
 	_has_data = true;
-	reti(); // retorna de uma rotina de interrupção
 }
 
-void UART::tx_isr_handler(USART0_UDRE_vect){
+void UART::tx_isr_handler(){
 	UDR0 = _tx_buffer;
 	UCSR0B &= ~(1<<UDRIE0);
-	reti();
+}
+
+ISR(USART0_RX_vect){
+	UART::rx_isr_handler();
+}
+
+ISR(USART0_UDRE_vect){
+	UART::tx_isr_handler();
 }
